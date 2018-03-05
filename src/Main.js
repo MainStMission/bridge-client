7c6a9c19c6e514acb8fdd0673da5a2f0349acfaa
tbrooke <tom.brooke@gmail.com>
2018-03-04 19:37:08 -0500

    added material-ui material-ui@next beta version

--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,26 @@
 import React, { Component } from "react";
+var FontAwesome = require('react-fontawesome');
 
 class Main extends Component {
     render() {
-    return (
+        return (
         <div>
-            <h1> Main Street Mission </h1>
-                <ul className="header">
-                    <li><a href= "/">Households</a></li>
+          <div id="menu-outer">
+            <h1> Main St Mission </h1>
+            <div class="menu-table">
+                <ul id="navbar">
+                    <li><FontAwesome name='user-plus' /> <a href= "/"> Households</a></li>
                     <li><a href= "/">Donations</a></li>
                     <li><a href= "/">Volunteers</a></li>
                     <li><a href= "/">Admin</a></li>
-                    <li><a href= "/">Profile</a></li>
+                    <li id="prof"><a href= "/">Profile</a></li>
                 </ul>
-                <div className="Content">
+            </div>
+          </div>
+             <div className="Content">
 
-                </div>
-        </div>
+             </div>
+       </div>
        );
     }
 }
