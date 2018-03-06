14919c13bc5e31635426b93627a3451cf8519155
tbrooke <tom.brooke@gmail.com>
2018-03-05 19:40:28 -0500

    added styled components and dicty menu code

---
+++ src/Components/Brand.js
@@ -0,0 +1,43 @@
+// @flow
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  list-style-type: none;
+  display: flex;
+  padding: 10px;
+
+  @media (max-width: 768px) {
+    height: 30px;
+  }
+`
+const Link = styled.a`
+  display: block;
+  margin: auto;
+  text-decoration: none;
+  color: ${ props => props.theme.text ? props.theme.text : 'white' };
+  font-size: 1.5em;
+
+  @media (max-width: 768px) {
+    top: 0;
+    z-index: 10;
+  }
+`
+
+type Props = {
+    title: String,
+    href: String
+}
+
+export default class Brand extends Component {
+    displayName = 'Brand'
+    props: Props
+    render() {
+        const { title, href } = this.props
+        return (
+            <Container>
+              <Link href={ href }>{ title }</Link>
+            </Container>
+        )
+    }
+}
