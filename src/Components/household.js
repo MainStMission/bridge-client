62862298ce7e587a39306412b40d7a9d9d8a236e
tbrooke <tom.brooke@gmail.com>
2018-03-07 20:40:09 -0500

    barebones Household page

---
+++ src/Components/household.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Render from 'react-dom';
+import { formik } from 'formik';
+import Yup from 'yup';
+
+
+const Household = () => (
+    <div>
+Households
+    </div>
+);
+
+render(<Household />, document.getElementById('root'));
+
+export default Household;
